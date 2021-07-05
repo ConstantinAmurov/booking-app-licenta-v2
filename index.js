f0a24b7bde8b5d6f01ab44af9b5cbb7804088de0
CostelMD <amurov.constantin@gmail.com>
2021-07-05 17:44:29 +0300

    CHANGED VAR NAME

--- index.js
+++ index.js
@@ -1,38 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
 //conectare MONGODB
-const uri = process.env.ARLAS_URI;
+const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
 });
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('Mongo DB connect success');
+connection.once("open", () => {
+  console.log("Mongo DB connect success");
 });
 
-const authRouter = require('./routes/auth');
-const CompanyRouter = require('./routes/companies')
+const authRouter = require("./routes/auth");
+const CompanyRouter = require("./routes/companies");
 
+app.use("/api/auth", authRouter);
+app.use("/api/companies", CompanyRouter);
 
-app.use('/api/auth',authRouter);
-app.use('/api/companies',CompanyRouter);
-
-
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
 app.listen(port, () => {
-    console.log(`Example app listening on port port!`)
+  console.log(`Example app listening on port port!`);
 });
-
