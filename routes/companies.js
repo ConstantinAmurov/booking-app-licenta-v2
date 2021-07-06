0611712944e04a8cf9e6061d7c1f75657a162e32
CostelMD <amurov.constantin@gmail.com>
2021-07-06 15:38:51 +0300

    Fixed some bugs

--- routes/companies.js
+++ routes/companies.js
@@ -7,287 +7,269 @@ const { reservationValidate } = require("../validation");
 const checkToken = require("./verifyToken");
 const router = require("express").Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    const findCompanies = await Company.find();
 
+    res.status(200).json({
+      succes: true,
+      companies: [...findCompanies],
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      succes: false,
+      error: "Not authorized",
+    });
+  }
+});
+
+router.post("/add", checkToken, async (req, res) => {
+  const user = req.user;
+
+  const { name, imageURL, description } = req.body;
+  const findCompanie = await Company.findOne({ name });
+  if (findCompanie) {
+    return res.status(400).json({
+      succes: false,
+      error: "Company name already exist",
+    });
+  }
+
+  const newCompany = new Company({
+    name,
+    imageURL,
+    description,
+    userID: user._id,
+  });
+  const savedCompany = await newCompany.save();
+
+  return res.status(200).json({
+    succes: true,
+    message: "Company was added",
+    data: savedCompany,
+  });
+});
+
+router.post("/edit/:companyID", checkToken, async (req, res) => {
+  const user = req.user;
+  const { companyID } = req.params;
+
+  if (!isValidObjectId(companyID)) {
+    return res.status(200).json({
+      succes: false,
+      error: "Incorrect company ID",
+    });
+  }
+
+  const { name, imageURL, description } = req.body;
+  const findCompanie = await Company.findById(companyID);
+  if (!!findCompanie) {
+    if (findCompanie.userID == user._id) {
+      findCompanie.name = name;
+      findCompanie.imageURL = imageURL;
+      findCompanie.description = description;
+      const savedCompany = await findCompanie.save();
+
+      return res.status(200).json({
+        succes: true,
+        message: "Company was changed",
+        company: savedCompany,
+      });
+    }
+  }
+
+  return res.status(200).json({
+    succes: true,
+    message: "You don't have this company",
+  });
+});
+
+router.delete("/delete/:companyID", checkToken, async (req, res) => {
+  const user = req.user;
+  const { companyID } = req.params;
+
+  if (!isValidObjectId(companyID)) {
+    return res.status(200).json({
+      succes: false,
+      error: "Incorrect company ID",
+    });
+  }
+
+  const findCompanie = await Company.findById(companyID);
+  if (findCompanie) {
+    if (findCompanie.userID == user._id) {
+      await Company.findByIdAndDelete(companyID);
+
+      return res.status(200).json({
+        succes: true,
+        message: "Company was deleted",
+      });
+    }
+  }
+  return res.status(200).json({
+    succes: true,
+    message: "You don't have this company",
+  });
+});
+
+router.delete(
+  "/deleteService/:companyID/:serviceID",
+  checkToken,
+  async (req, res) => {
+    const user = req.user;
+
+    const { companyID, serviceID } = req.params;
+
+    if (!isValidObjectId(companyID) || !isValidObjectId(serviceID)) {
+      return res.status(200).json({
+        succes: false,
+        error: "Incorrect company ID",
+      });
+    }
+
+    const findCompanie = await Company.findById(companyID);
+    if (findCompanie) {
+      findCompanie.services = findCompanie.services.filter((service) => {
+        return service._id.toString() != serviceID;
+      });
+      const savedCompany = await findCompanie.save();
+      return res.status(200).json({
+        succes: true,
+        message: "Service was deleted",
+      });
+    }
+    return res.status(200).json({
+      succes: true,
+      message: "You don't have this company",
+    });
+  }
+);
+
+router.post("/addService/:companyID", checkToken, async (req, res) => {
+  const user = req.user;
+  const { companyID } = req.params;
+  const { services } = req.body;
+  try {
+    const findCompanie = await Company.findById(companyID);
+    if (findCompanie) {
+      findCompanie.services.push(...services);
+      const savedCompany = await findCompanie.save();
+      return res.status(200).json({
+        succes: true,
+        message: "Service was added",
+      });
+    }
+    return res.status(400).json({
+      succes: false,
+      message: "Service error",
+    });
+  } catch (err) {
+    return res.status(200).json({
+      succes: false,
+      message: err,
+    });
+  }
+});
+
+router.post("/editServices/:companyID", checkToken, async (req, res) => {
+  const user = req.user;
+  const { companyID } = req.params;
+  const { services, servicesID } = req.body;
+  try {
+    const findCompanie = await Company.findById(companyID);
+    if (findCompanie) {
+      findCompanie.services = findCompanie.services.map((service) => {
+        const searchServiceIndex = servicesID.indexOf(service._id.toString());
+        if (searchServiceIndex !== -1) {
+          services[searchServiceIndex].periods.byGuests =
+            service.periods.byGuests;
+          return services[searchServiceIndex];
+        }
+        return service;
+      });
+      const savedCompany = await findCompanie.save();
+      return res.status(200).json({
+        succes: true,
+        message: "Service was changed",
+      });
+    }
+    return res.status(400).json({
+      succes: false,
+      message: "Service error",
+    });
+  } catch (err) {
+    return res.status(200).json({
+      succes: false,
+      message: err,
+    });
+  }
+});
+
+router.post("/makeReservation/:companyID/:serviceID", async (req, res) => {
+  const { companyID, serviceID } = req.params;
+
+  const { email, firstName, guestNumber, lastName, phone, time } = req.body;
+
+  if (!isValidObjectId(companyID) || !isValidObjectId(serviceID)) {
+    return res.status(200).json({
+      succes: false,
+      error: "Incorrect company/service ID",
+    });
+  }
+
+  const { error } = reservationValidate(req.body);
+  if (error) {
+    return res.status(400).json({
+      succes: false,
+      error: error.details[0].message,
+    });
+  }
+
+  const findCompanie = await Company.findById(companyID);
+  if (findCompanie) {
+    findCompanie.services = findCompanie.services.map((service) => {
+      if (service._id.toString() === serviceID) {
+        service.periods.byGuests.push({
+          ...req.body,
+        });
+      }
+      return service;
+    });
+    debugger;
+    const reservMessage = `Hello ${lastName},<br> You got a new message from <b>Booking App</b>:<br> Thank you for booking your service with us!! Your reservation for company: ${
+      findCompanie.name
+    } on ${new Date(time)
+      .toString()
+      .split(" ")
+      .slice(0, 5)
+      .join(" ")
+      .split(":")
+      .slice(0, 2)
+      .join(":")}. please find the details below`;
     try {
-        const findCompanies = await Company.find();
-
-        res.status(200).json({
-            succes: true,
-            companies: [...findCompanies]
-
-        })
+      await sendMail({
+        to: email,
+        subject: "New message from Booking App",
+        text: reservMessage,
+      });
     } catch (err) {
-
-        console.log(err)
-        res.status(400).json({
-            succes: false,
-            error: "Not authorized"
-        })
-
+      res.status(400).json({
+        succes: false,
+        error: "Email could not be send",
+      });
     }
+
+    const savedCompany = await findCompanie.save();
+    return res.status(200).json({
+      succes: true,
+      message: reservMessage,
+    });
+  }
+  return res.status(200).json({
+    succes: true,
+    message: "Inexistent company/service",
+  });
 });
-
-router.post('/add', checkToken, async (req, res) => {
-    const user = req.user;
-
-        const { name, imageURL, description } = req.body;
-        const findCompanie = await Company.findOne({ name });
-        if (findCompanie) {
-            return res.status(400).json({
-                succes: false,
-                error: "Company name already exist"
-            })
-        }
-
-        const newCompany = new Company({ name, imageURL, description, userID:user._id });
-        const savedCompany = await newCompany.save();
-
-        return res.status(200).json({
-            succes: true,
-            message: "Company was added",
-            data: savedCompany,
-        })
-    
-})
-
-router.post('/edit/:companyID', checkToken, async (req, res) => {
-    const user = req.user;
-    const {companyID} = req.params;
-
-    if(!isValidObjectId(companyID)){
-        return res.status(200).json({
-            succes: false,
-            error: "Incorrect company ID",
-        });
-     }
-
-        const { name, imageURL, description } = req.body;
-        const findCompanie = await Company.findById(companyID);
-        if (!!findCompanie) {
-            if(findCompanie.userID == user._id){
-                
-                    
-                findCompanie.name = name;
-                findCompanie.imageURL = imageURL;
-                findCompanie.description = description;                
-                const savedCompany =  await findCompanie.save()
-                
-                return res.status(200).json({
-                    succes: true,
-                    message: "Company was changed",
-                company: savedCompany,
-            })
-
-    }
-}
-
-    return res.status(200).json({
-        succes: true,
-        message: "You don't have this company",
-    })
-    
-})
-
-
-router.delete('/delete/:companyID', checkToken, async (req, res) => {
-    const user = req.user;
-    const {companyID} = req.params;
-
-    if(!isValidObjectId(companyID)){
-        return res.status(200).json({
-            succes: false,
-            error: "Incorrect company ID",
-        })
-     }
-
-    const findCompanie = await Company.findById(companyID);
-    if (findCompanie) {
-        if(findCompanie.userID == user._id){
-
-        await Company.findByIdAndDelete(companyID)
-        
-            return res.status(200).json({
-                succes: true,
-                message: "Company was deleted",
-            })
-    }
-}
-    return res.status(200).json({
-        succes: true,
-        message: "You don't have this company",
-    })
-    
-});
-
-router.delete('/deleteService/:companyID/:serviceID', checkToken, async (req, res) => {
-    const user = req.user;
-
-    const {companyID,serviceID} = req.params;
-
-    if(!isValidObjectId(companyID) || !isValidObjectId(serviceID)){
-        return res.status(200).json({
-            succes: false,
-            error: "Incorrect company ID",
-        });
-     }
-
-    const findCompanie = await Company.findById(companyID);
-    if (findCompanie) {
-
-         findCompanie.services = findCompanie.services.filter(service=>{
-             
-                
-                return service._id.toString() != serviceID
-            })
-            const savedCompany = await findCompanie.save();
-            return res.status(200).json({
-                succes: true,
-                message: "Service was deleted",
-            });
-    }
-    return res.status(200).json({
-        succes: true,
-        message: "You don't have this company",
-    })
-    
-});
-
-router.post('/addService/:companyID', checkToken, async (req, res) => {
-    const user = req.user;
-    const {companyID} = req.params;
-    const { services} = req.body;
-    try{
-
-        const findCompanie = await Company.findById(companyID);
-        if (findCompanie) {
-            
-            findCompanie.services.push(...services)
-            const savedCompany = await findCompanie.save();
-            return res.status(200).json({
-                succes: true,
-                message: "Service was added",
-            });
-
-            
-            
-        }
-        return res.status(400).json({
-            succes: false,
-            message: "Service error",
-        });
-    }catch(err){
-        return res.status(200).json({
-            succes: false,
-            message: err,
-        });
-    }
-})
-
-router.post('/editServices/:companyID', checkToken, async (req, res) => {
-    const user = req.user;
-    const {companyID} = req.params;
-    const { services,servicesID} = req.body;
-    try{
-
-        const findCompanie = await Company.findById(companyID);
-        if (findCompanie) {
-            
-            findCompanie.services = findCompanie.services.map(service=>{
-                const searchServiceIndex = servicesID.indexOf(service._id.toString());
-                if(searchServiceIndex!==-1){
-
-                    services[searchServiceIndex].periods.byGuests = service.periods.byGuests;
-                    return  services[searchServiceIndex]
-                }
-                return service
-
-
-            })
-            const savedCompany = await findCompanie.save();
-            return res.status(200).json({
-                succes: true,
-                message: "Service was changed",
-            });
-
-            
-            
-        }
-        return res.status(400).json({
-            succes: false,
-            message: "Service error",
-        });
-    }catch(err){
-        return res.status(200).json({
-            succes: false,
-            message: err,
-        });
-    }
-})
-
-router.post('/makeReservation/:companyID/:serviceID', async (req, res) => {
-    const {companyID,serviceID} = req.params;
-
-    const {email,firstName,guestNumber,lastName,phone,time} = req.body;
-
-
-    if(!isValidObjectId(companyID) || !isValidObjectId(serviceID)){
-        return res.status(200).json({
-            succes: false,
-            error: "Incorrect company/service ID",
-        });
-    }
-
-
-    
-    const { error } = reservationValidate(req.body);
-    if (error) {
-        return res.status(400).json({
-            succes: false,
-            error: error.details[0].message,
-        });
-    }
-    
-
-    const findCompanie = await Company.findById(companyID);
-    if (findCompanie) {
-
-         findCompanie.services = findCompanie.services.map(service=>{
-
-                if(service._id.toString() === serviceID){
-                    service.periods.byGuests.push({
-                        ...req.body,
-                    })
-                }
-                return service
-
-            })
-            const reservMessage = `Hi ${lastName}, Appointment confirmed with company ${findCompanie.name} on ${new Date(time).toString().split(' ').slice(0,5).join(' ').split(':').slice(0,2).join(':')}. please find the details below`
-            try {
-                await sendMail({
-                    to: email,
-                    subject: "Confirm Reservation",
-                    text: reservMessage,
-                })
-            } catch (err) {
-    
-                res.status(400).json({
-                    succes: false,
-                    error: 'Email could not be send'
-                })
-            }
-
-            const savedCompany = await findCompanie.save();
-            return res.status(200).json({
-                succes: true,
-                message: reservMessage,
-            });
-    }
-    return res.status(200).json({
-        succes: true,
-        message: "Inexistent company/service",
-    })
-})
-
 
 module.exports = router;
