32b38f57eb9e28c308ce39d061fdfade384904cd
CostelMD <amurov.constantin@gmail.com>
2021-07-06 09:12:09 +0300

    Reverted and added new connection in index js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,15 +12,22 @@ app.use(express.json());
 const uri =
   process.env.MONGODB_URI ||
   "mongodb+srv://admin:costel079616617@cluster0.rvuou.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Database connected successfully!");
+  })
+  .catch((err) => {
+    console.log("Error connecting with error code:", err);
+  });
 const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("Mongo DB connect success");
-});
+// connection.once("open", () => {
+//   console.log("Mongo DB connect success");
+// });
 
 const authRouter = require("./routes/auth");
 const CompanyRouter = require("./routes/companies");
@@ -28,9 +35,10 @@ const CompanyRouter = require("./routes/companies");
 app.use("/api/auth", authRouter);
 app.use("/api/companies", CompanyRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+app.use(express.static("client/build"));
+app.get("*", (req, res) => {
+  res.sendFile("client/build");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port port!`);
